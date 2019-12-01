8a73f52a9733e947d94fa6040a4e4cdbe717c891
PetteriM1 <petteri.malkavaara1@gmail.com>
2019-12-01 17:06:37 +0200

    Improved version

--- src/main/java/suomicraftpe/events/christmas/Main.java
+++ src/main/java/suomicraftpe/events/christmas/Main.java
@@ -1,11 +1,16 @@
 package suomicraftpe.events.christmas;
 
+import cn.nukkit.Player;
 import cn.nukkit.event.EventHandler;
 import cn.nukkit.event.Listener;
+import cn.nukkit.event.entity.EntityLevelChangeEvent;
 import cn.nukkit.event.level.ChunkLoadEvent;
-import cn.nukkit.level.Level;
+import cn.nukkit.event.level.WeatherChangeEvent;
+import cn.nukkit.event.player.PlayerJoinEvent;
+import cn.nukkit.network.protocol.LevelEventPacket;
 import cn.nukkit.plugin.PluginBase;
-import cn.nukkit.utils.Config;
+
+import java.util.List;
 
 /**
  * SuomiCraft PE Events / Christmas
@@ -13,45 +18,69 @@ import cn.nukkit.utils.Config;
  */
 public class Main extends PluginBase implements Listener {
 
-    public static Config config;
+    private static int mode;
+    private static List<String> worlds;
 
     @Override
     public void onEnable() {
         saveDefaultConfig();
-        config = getConfig();
+        mode = getConfig().getInt("mode");
+        worlds = getConfig().getStringList("worlds");
         getServer().getPluginManager().registerEvents(this, this);
+    }
 
-        for (String s : config.getStringList("worlds")) {
-            Level l = getServer().getLevelByName(s);
-
-            if (l != null) {
-                if (config.getInt("mode") == 0) {
-                    l.setRaining(true);
-                } else if (config.getInt("mode") == 1) {
-                    l.setRaining(false);
+    @EventHandler
+    public void onChunkLoad(ChunkLoadEvent e) {
+        if (worlds.contains(e.getLevel().getName())) {
+            if (mode == 0) {
+                for (int x = 0; x < 16; x++) {
+                    for (int z = 0; z < 16; z++) {
+                        e.getChunk().setBiomeId(x, z, 12);
+                    }
+                }
+            } else if (mode == 1) {
+                for (int x = 0; x < 16; x++) {
+                    for (int z = 0; z < 16; z++) {
+                        e.getChunk().setBiomeId(x, z, 1);
+                    }
                 }
             }
         }
     }
 
     @EventHandler
-    public void onChunkLoad(ChunkLoadEvent e) {
-        if (!config.getStringList("worlds").contains(e.getLevel().getName())) {
-            return;
+    public void onWeatherChange(WeatherChangeEvent e) {
+        if (mode == 0 && worlds.contains(e.getLevel().getName())) {
+            e.setCancelled();
         }
+    }
 
-        if (config.getInt("mode") == 0) {
-            for (int x = 0; x < 16; x++) {
-                for (int z = 0; z < 16; z++) {
-                    e.getChunk().setBiomeId(x, z, 12);
-                }
+    @EventHandler
+    public void onJoin(PlayerJoinEvent e) {
+        if (worlds.contains(e.getPlayer().getLevel().getName())) {
+            setRaining(e.getPlayer());
+        }
+    }
+
+    @EventHandler
+    public void onLevelChange(EntityLevelChangeEvent e) {
+        if (e.getEntity() instanceof Player) {
+            if (worlds.contains(e.getTarget().getName())) {
+                setRaining((Player) e.getEntity());
             }
-        } else if (config.getInt("mode") == 1) {
-            for (int x = 0; x < 16; x++) {
-                for (int z = 0; z < 16; z++) {
-                    e.getChunk().setBiomeId(x, z, 1);
-                }
-            }
+        }
+    }
+
+    private void setRaining(Player p) {
+        if (mode == 0) {
+            getServer().getScheduler().scheduleDelayedTask(this, () -> {
+                try {
+                    LevelEventPacket pk = new LevelEventPacket();
+                    pk.evid = LevelEventPacket.EVENT_START_RAIN;
+                    pk.data = Integer.MAX_VALUE;
+                    p.dataPacket(pk);
+                } catch (Exception ignore) {}
+            }, 20);
         }
     }
 }
