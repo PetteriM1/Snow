772e05067090632c5222c0ccc331ae11efb26abf
PetteriM1 <petteri.malkavaara1@gmail.com>
2020-12-31 16:24:46 +0200

    Patch BiomeDefinitionListPacket so no biome changes required

--- src/main/java/suomicraftpe/events/christmas/Main.java
+++ src/main/java/suomicraftpe/events/christmas/Main.java
@@ -7,9 +7,17 @@ import cn.nukkit.event.entity.EntityLevelChangeEvent;
 import cn.nukkit.event.level.ChunkLoadEvent;
 import cn.nukkit.event.level.WeatherChangeEvent;
 import cn.nukkit.event.player.PlayerJoinEvent;
+import cn.nukkit.nbt.NBTIO;
+import cn.nukkit.nbt.tag.CompoundTag;
+import cn.nukkit.nbt.tag.Tag;
+import cn.nukkit.network.protocol.BiomeDefinitionListPacket;
 import cn.nukkit.network.protocol.LevelEventPacket;
 import cn.nukkit.plugin.PluginBase;
 
+import java.io.ByteArrayInputStream;
+import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
+import java.nio.ByteOrder;
 import java.util.List;
 
 /**
@@ -19,19 +27,45 @@ import java.util.List;
 public class Main extends PluginBase implements Listener {
 
     private static int mode;
+    private static boolean legacy;
     private static List<String> worlds;
 
     @Override
     public void onEnable() {
         saveDefaultConfig();
         mode = getConfig().getInt("mode");
+        legacy = getConfig().getBoolean("legacy");
         worlds = getConfig().getStringList("worlds");
         getServer().getPluginManager().registerEvents(this, this);
+
+        if (!legacy) {
+            if (mode != 0) {
+                getLogger().error("The 'mode' setting is only available in the legacy mode");
+            }
+            try {
+                new BiomeDefinitionListPacket();
+                Class<?> c_BiomeDefinitionListPacket = Class.forName("cn.nukkit.network.protocol.BiomeDefinitionListPacket");
+                Field f_TAG = c_BiomeDefinitionListPacket.getDeclaredField("TAG");
+                f_TAG.setAccessible(true);
+                Field f_modifiers = Field.class.getDeclaredField("modifiers");
+                f_modifiers.setAccessible(true);
+                f_modifiers.setInt(f_TAG, f_TAG.getModifiers() & ~Modifier.FINAL);
+                byte[] TAG = (byte[]) f_TAG.get(null);
+                Tag compoundTag = NBTIO.readTag(new ByteArrayInputStream(TAG), ByteOrder.BIG_ENDIAN, true);
+                for (Tag tag : ((CompoundTag) compoundTag).getAllTags()) {
+                    ((CompoundTag) tag).putFloat("temperature", -0.5f);
+                }
+                TAG = NBTIO.writeNetwork(compoundTag);
+                f_TAG.set(null, TAG);
+            } catch (Exception ex) {
+                getLogger().error("Failed to patch BiomeDefinitionListPacket", ex);
+            }
+        }
     }
 
     @EventHandler
     public void onChunkLoad(ChunkLoadEvent e) {
-        if (worlds.contains(e.getLevel().getName())) {
+        if (legacy && worlds.contains(e.getLevel().getName())) {
             if (mode == 0) {
                 for (int x = 0; x < 16; x++) {
                     for (int z = 0; z < 16; z++) {
@@ -51,36 +85,32 @@ public class Main extends PluginBase implements Listener {
     @EventHandler
     public void onWeatherChange(WeatherChangeEvent e) {
         if (mode == 0 && worlds.contains(e.getLevel().getName())) {
-            e.setCancelled();
+            e.setCancelled(true);
         }
     }
 
     @EventHandler
     public void onJoin(PlayerJoinEvent e) {
-        if (worlds.contains(e.getPlayer().getLevel().getName())) {
-            setRaining(e.getPlayer());
-        }
+        setRaining(e.getPlayer());
     }
 
     @EventHandler
     public void onLevelChange(EntityLevelChangeEvent e) {
         if (e.getEntity() instanceof Player) {
-            if (worlds.contains(e.getTarget().getName())) {
-                setRaining((Player) e.getEntity());
-            }
+            setRaining((Player) e.getEntity());
         }
     }
 
     private void setRaining(Player p) {
-        if (mode == 0) {
+        if (mode == 0 && worlds.contains(p.getLevel().getName())) {
             getServer().getScheduler().scheduleDelayedTask(this, () -> {
-                try {
+                if (p.isOnline() && worlds.contains(p.getLevel().getName())) {
                     LevelEventPacket pk = new LevelEventPacket();
                     pk.evid = LevelEventPacket.EVENT_START_RAIN;
-                    pk.data = Integer.MAX_VALUE;
+                    pk.data = 6000000;
                     p.dataPacket(pk);
-                } catch (Exception ignore) {}
-            }, 20);
+                }
+            }, 10);
         }
     }
 }
